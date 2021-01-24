e90e0e17142e697a7f38b9b2626f2b294db5ce83
marian <marian@localhost.localdomain>
2021-01-24 16:35:34 +0100

    ex9 and ex10

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,13 +1,25 @@
 const
     router = require('express').Router(),
-    jsonData = require('./dati.json')
-router.get('/getImporti', (req, res) => {
-    res.status(200).send(jsonData)
+    multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, ".")
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname)
+    },
 })
-router.post('/nuovoImporto', (req, res) => {
-    console.log("POST")
-    jsonData.push(req.body)
-    console.log(jsonData)
-    res.status(200).send(jsonData)
+const uploadStorage = multer({ storage: storage })
+router.post("/uploadImage", uploadStorage.single("file"), (req, res) => {
+    return res.status(200).send(req.file)
+})
+router.get('/getImage', (req, res) => {
+    const { filename } = req.body
+    if (typeof filename === "undefined") {
+        res.status(404).send('NOT FOUND')
+    } else {
+        res.status(200).sendFile(filename)
+    }
 })
 module.exports = router
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,40 @@
-import Imports from './Imports'
-import 'bootstrap/dist/css/bootstrap.min.css';
-const App = () => {
-    return (
-        <div>
-            <Imports ></Imports>
-        </div>
-    )
+import { Component } from 'react'
+import axios from 'axios'
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedFile: undefined,
+            pathToImage: undefined
+        }
+    }
+    handleFileChange = e => {
+        this.setState({
+            selectedFile: e.target.files[0]
+        })
+    }
+    handleSubmit = () => {
+        const { selectedFile, pathToImage } = this.state
+        axios.post("http://localhost:4000/uploadImage", {
+            "file": selectedFile
+        }).then(response => {
+            console.log(response)
+            this.setState({
+                pathToImage: response.data.path
+            })
+        }).catch(err => {
+            if (err) { return err }
+        })
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="file" onChange={this.handleFileChange}>Choose Image</input>
+                    <input type="submit" >Upload Image</input>
+                </form>
+                <img src={this.state.pathToImage}></img>
+            </div>
+        )
+    }
 }
-export default App
